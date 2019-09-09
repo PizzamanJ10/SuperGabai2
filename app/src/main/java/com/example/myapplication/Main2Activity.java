9f8fa085cfd358384ca1f3efcf45baec63ea1ef6
PizzamanJ10 <55109111+PizzamanJ10@users.noreply.github.com>
2019-09-09 23:08:26 +0300

    Hello

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button button;
+    private EditText Name;
+    private EditText Stat;
+    private EditText Boolean;
+    private String NameST;
+    private String had;
+    private String statST;
+    private int stat;
+    private int counter = 0 ;
+    private guys[] allguys;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        allguys = new guys[65];
+
+        Name = findViewById(R.id.edittextName);
+        Stat = findViewById(R.id.edittextStatus);
+        Boolean = findViewById(R.id.edittextHad);
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click();
+            }
+        });
+
+    }
+
+    private void click() {
+        NameST = Name.getText().toString();
+        statST = Stat.getText().toString();
+        had = Boolean.getText().toString();
+         boolean pizza = (had.equals("true") );
+        int finalValue = Integer.parseInt(statST);
+        allguys[counter] = new guys(NameST,finalValue,pizza);
+        Toast.makeText(this, pizza + "", Toast.LENGTH_SHORT).show();
+        counter++;
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/guys.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+public class guys {
+
+        private boolean hadAliyah;
+        private String name;
+        private int Status;
+
+        public guys (String nm, int stat, boolean had ){
+            this.name = nm;
+            this.Status = stat;
+            this.hadAliyah = had;
+        }
+
+
+}

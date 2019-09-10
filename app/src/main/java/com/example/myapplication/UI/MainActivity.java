784a1102c4994300cb418acce21104574360bd95
akivav <akiva.grobman@gmail.com>
2019-09-10 18:56:35 +0300

    started the data-base

--- app/src/main/java/com/example/myapplication/UI/MainActivity.java
+++ app/src/main/java/com/example/myapplication/UI/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,7 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
